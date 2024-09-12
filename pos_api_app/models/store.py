5b31c3a510a7f83be07f4677f3277f4daa787159
Ferhat Mousavi <ferhat.mousavi@gmail.com>
2024-09-12 20:57:38 +0300

    contacts added to store model.

--- pos_api_app/models/store.py
+++ pos_api_app/models/store.py
@@ -39,6 +39,9 @@ class Store(models.Model):
     # Indicates if the store is active or closed
     is_active = models.BooleanField(default=True)
 
+    # Foreign key to the Contact model, to link one or more contact persons associated with the store
+    contacts = models.ManyToManyField('Contact', related_name='merchants')
+
     # Foreign key to the User model, represents the user who created this store record
     created_by = models.ForeignKey(User, related_name='store_created', on_delete=models.SET_NULL, null=True, blank=True)
 
